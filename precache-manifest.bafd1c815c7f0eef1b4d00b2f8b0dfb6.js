397f8758e64113b1c547efa2b6acf9dd77f0c069
Caterina <caterina.pampolari@live.it>
2020-04-19 19:20:11 +0200

    Updates

---
+++ precache-manifest.bafd1c815c7f0eef1b4d00b2f8b0dfb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5113f21169cc0403f7d12aacb4449e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b361ff42191403029100",
+    "url": "/robofriends/static/css/main.673f3442.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b361ff42191403029100",
+    "url": "/robofriends/static/js/main.79d3c2e8.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
